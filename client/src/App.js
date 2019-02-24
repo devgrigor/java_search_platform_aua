f3b118ddcf9840055f44d7e86e717b452f43f298
devgrigor <dev.grigor@gmail.com>
2019-02-24 18:42:35 +0400

    added react app in client folder

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        	<p>GALA Seatch is still in development</p>
+			<img src={logo} className="App-logo" alt="logo" />
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
