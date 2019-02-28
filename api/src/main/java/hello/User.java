99065b3aca786ee39b04bd113da1f1c827bb871d
devgrigor <dev.grigor@gmail.com>
2019-02-28 09:54:55 +0400

    added redis caching, not well testet yet

--- api/src/main/java/hello/User.java
+++ api/src/main/java/hello/User.java
@@ -4,9 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
+import org.springframework.data.redis.core.RedisHash;
 
+
+@RedisHash("User") // This tells redis that the user is cachable
 @Entity // This tells Hibernate to make a table out of this class
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
